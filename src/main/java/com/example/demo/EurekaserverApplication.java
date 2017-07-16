194bddeef02ca27aa9fe54c6075369a9f7a0e8b2
Ashish Garg <agarg23@sapient.com>
2017-07-16 07:35:27 +0530

    zz

--- src/main/java/com/example/demo/EurekaserverApplication.java
+++ src/main/java/com/example/demo/EurekaserverApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
+		SpringApplication.run(EurekaserverApplication.class, args); 
 	}
 }
